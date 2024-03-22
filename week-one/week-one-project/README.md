03a6863e8e9e6abf11ca2d0994ab34ef35be3a94
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-03-22 16:27:02 +0000

    fridays version, with style and JS

--- week-one/week-one-project/README.md
+++ week-one/week-one-project/README.md
@@ -1,6 +1,6 @@
 # Week One Project
 
-## Project- Build out the website!##
+## Project- Build out the website!
 
 I've chosen to build my website around an eco-friendly campsite opening in Spain.
 
@@ -30,7 +30,7 @@ I quite liked how the header stayed fixed, and thought the translucent backgroun
 I struggled to find a happy solution to the 'width' of my header. Upon 'inspecting' that element, the padding shows on the top, left, and bottom but I can't see the right edge of the headers padding.
 Maybe this is characteristic of it taking up 100% of the page width.
 
-**Anchors**
+## Anchors
 
 I edited the styling of the buttons in the header, namely giving them a distinct look with a different colour and made them more noticeable with a border.
 
@@ -45,14 +45,21 @@ The footer is very basic. Keeping it simple!
 ### JavaScript
 
 **Changing background images**
+
 As a stretch, I attempted to add some buttons to allow the user to change the background images they see.
+
 Firstly, this discussion let me design the buttons on the page how I wanted them: https://stackoverflow.com/questions/38320878/circle-button-css
+
 I found the correct syntax I needed to access the backgroundImage property here: https://www.w3schools.com/cssref/tryit.php?filename=trycss_js_background-image
+
 I wrote out some javascript which declared variables attached to the buttons and page sections, added 'mouse over' colour changes, and finally changed the background images of the sections. This worked great!
 
 **If-statements for mouseover events**
+
 I really had to spend a lot of time fiddling around with an 'if' statement to try to keep the button colours as they should be, depending on which image was set.
+
 I used ChatGPT to look over the code I had written (which I've retained in app.js but commented out). It suggested a tidier if-statement which I ended up using, however, it wasn't complete!
+
 I had to add an extra 'else if' to it's suggested code, to make sure the colours were correct in all states.
 
 Following that I duplicated the code so that each section had buttons, and the buttons changed the background (I left out duplicating the 'mouseover' code until we do functions!)
--- week-one/week-one-project/app.js
+++ week-one/week-one-project/app.js
@@ -1,1 +1,131 @@
-console.log("Are we here?");
+// decalring some variables
+// first, the buttons
+const introBtnOne = document.getElementById("introBtnOne");
+const introBtnTwo = document.getElementById("introBtnTwo");
+const storyBtnOne = document.getElementById("storyBtnOne");
+const storyBtnTwo = document.getElementById("storyBtnTwo");
+const contactBtnOne = document.getElementById("contactBtnOne");
+const contactBtnTwo = document.getElementById("contactBtnTwo");
+// next the page sections
+const introSection = document.getElementById("introSection");
+const storySection = document.getElementById("storySection");
+const contactSection = document.getElementById("contactSection");
+
+// these next lines set the initial button colour to show which image the user is currently on.
+introBtnOne.style.backgroundColor = "darkolivegreen";
+storyBtnOne.style.backgroundColor = "darkolivegreen";
+contactBtnOne.style.backgroundColor = "darkolivegreen";
+
+// these variables
+let introImg = 0;
+let storyImg = 0;
+let contactImg = 0;
+
+// this statements handle the change of image when the buttons are clicked
+// about section
+introBtnOne.addEventListener("click", function () {
+  introSection.style.backgroundImage = "url(./assets/campsite1.jpg)";
+  introBtnOne.style.backgroundColor = "darkolivegreen";
+  introBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  introImg = 0;
+});
+introBtnTwo.addEventListener("click", function () {
+  introSection.style.backgroundImage = "url(./assets/campsite4.jpg)";
+  introBtnOne.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  introBtnTwo.style.backgroundColor = "darkolivegreen";
+  introImg = 1;
+});
+// story section
+storyBtnOne.addEventListener("click", function () {
+  storySection.style.backgroundImage = "url(./assets/campsite2.jpg)";
+  storyBtnOne.style.backgroundColor = "darkolivegreen";
+  storyBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  storyImg = 0;
+});
+storyBtnTwo.addEventListener("click", function () {
+  storySection.style.backgroundImage = "url(./assets/campsite5.jpg)";
+  storyBtnOne.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  storyBtnTwo.style.backgroundColor = "darkolivegreen";
+  stroyImg = 1;
+});
+// contact section
+contactBtnOne.addEventListener("click", function () {
+  contactSection.style.backgroundImage = "url(./assets/campsite3.jpg)";
+  contactBtnOne.style.backgroundColor = "darkolivegreen";
+  contactBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  contactImg = 0;
+});
+contactBtnTwo.addEventListener("click", function () {
+  contactSection.style.backgroundImage = "url(./assets/campsite6.jpg)";
+  contactBtnOne.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  contactBtnTwo.style.backgroundColor = "darkolivegreen";
+  contactImg = 1;
+});
+
+// this very long if-statement (which hurts my eyes) was my attempt to ensure that the colour of the buttons remains as intended, to indicate to the user which image they're on.
+// it didn't work fully as intended, so I used ChatGPT to review my code and point out where I could make improvements. That revision is the working code underneath.
+// if (introImg === 0) {
+//   introBtnOne.addEventListener("mouseover", function () {
+//     introBtnOne.style.backgroundColor = "grey";
+//   });
+//   introBtnOne.addEventListener("mouseout", function () {
+//     introBtnOne.style.backgroundColor = "darkolivegreen";
+//   });
+//   introBtnTwo.addEventListener("mouseover", function () {
+//     introBtnTwo.style.backgroundColor = "grey";
+//   });
+//   introBtnTwo.addEventListener("mouseout", function () {
+//     introBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+//   });
+// } else if (introImg === 1) {
+//   introBtnOne.addEventListener("mouseover", function () {
+//     introBtnOne.style.backgroundColor = "grey";
+//   });
+//   introBtnOne.addEventListener("mouseout", function () {
+//     introBtnOne.style.backgroundColor = "(255, 255, 255, 0.75)";
+//   });
+//   introBtnTwo.addEventListener("mouseover", function () {
+//     introBtnTwo.style.backgroundColor = "grey";
+//   });
+//   introBtnTwo.addEventListener("mouseout", function () {
+//     introBtnTwo.style.backgroundColor = "red";
+//   });
+// }
+
+introBtnOne.addEventListener("mouseover", function () {
+  if (introImg === 0) {
+    introBtnOne.style.backgroundColor = "grey";
+    // here is the extra else-if I wrote in to ensure all colour states.
+  } else if (introImg === 1) {
+    introBtnOne.style.backgroundColor = "grey";
+  } else {
+    introBtnOne.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  }
+});
+
+introBtnOne.addEventListener("mouseout", function () {
+  if (introImg === 0) {
+    introBtnOne.style.backgroundColor = "darkolivegreen";
+  } else {
+    introBtnOne.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  }
+});
+
+introBtnTwo.addEventListener("mouseover", function () {
+  if (introImg === 1) {
+    introBtnTwo.style.backgroundColor = "grey";
+    // here is the extra else-if I wrote in to ensure all colour states.
+  } else if (introImg === 0) {
+    introBtnTwo.style.backgroundColor = "grey";
+  } else {
+    introBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  }
+});
+
+introBtnTwo.addEventListener("mouseout", function () {
+  if (introImg === 1) {
+    introBtnTwo.style.backgroundColor = "darkolivegreen";
+  } else {
+    introBtnTwo.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  }
+});
