1bb535c02b652f273660e37f1b526547712c7148
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-04-23 16:12:50 +0100

    added ternary operators

--- 06-week-six/button-count-demo/src/components/App.jsx
+++ 06-week-six/button-count-demo/src/components/App.jsx
@@ -8,6 +8,13 @@ import "/src/App.css";
 // import DecreaseButton from "./DecreaseButton";
 
 export default function App() {
+  const [showThings, setShowThings] = useState(false);
+
+  function handleShowThings() {
+    showThings ? setCount(count) : setCount(0);
+    setShowThings(!showThings);
+  }
+
   const [count, setCount] = useState(0);
 
   function increaseCount() {
@@ -19,21 +26,40 @@ export default function App() {
   }
 
   function resetCount() {
-    setCount(count - count);
+    setCount(0);
   }
 
   return (
     <section className="main-box">
       <h1>Hello all!</h1>
+      {showThings ? (
+        <Button className="stop-game" whenYouClickMe={handleShowThings}>
+          Stop playing? Resets your score!
+        </Button>
+      ) : (
+        <Button className="start-game" whenYouClickMe={handleShowThings}>
+          Do you want to play a game?
+        </Button>
+      )}
+
+      {/* Old code */}
       {/* <CountButton increaseCount={increaseCount} />
       <DecreaseButton decreaseCount={decreaseCount} /> */}
-      {/* New single-button-component code */}
-      <Button whenYouClickMe={increaseCount}>Increase me!</Button>
 
-      <Button whenYouClickMe={decreaseCount}>Decrease me!</Button>
+      {/* New single-button-component code, within a ternary operator*/}
 
-      <TextDisplay count={count} resetCount={resetCount} />
-      <p>but be careful if you click that number.....</p>
+      {showThings ? (
+        <>
+          <Button whenYouClickMe={increaseCount} count={count}>
+            Increase me!
+          </Button>
+          <Button whenYouClickMe={decreaseCount}>Decrease me!</Button>{" "}
+          <TextDisplay count={count} resetCount={resetCount} />
+          <p>but be careful if you click that number.....</p>
+        </>
+      ) : null}
+
+      {count > 10 ? <p>Oh my, you really are a clicker!</p> : null}
     </section>
   );
 }
--- 06-week-six/button-count-demo/src/components/Button.jsx
+++ 06-week-six/button-count-demo/src/components/Button.jsx
@@ -1,9 +1,13 @@
 import "/src/Button.css";
 
-export default function Button({ whenYouClickMe, children }) {
+export default function Button({ whenYouClickMe, className, count, children }) {
   return (
     <>
-      <button className="my-button" onClick={whenYouClickMe}>
+      <button
+        className={`my-button ${className ? className : ""}`}
+        onClick={whenYouClickMe}
+        style={count > 9 ? { backgroundColor: "orange" } : {}}
+      >
         {children}
       </button>
     </>
