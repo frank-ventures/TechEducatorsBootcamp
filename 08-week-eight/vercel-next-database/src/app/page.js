61aee404b0c628ce7c7e124a3c4889a36cd4478e
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-05-09 10:23:08 +0100

    added post data from vercel hosted database

--- 08-week-eight/vercel-next-database/src/app/page.js
+++ 08-week-eight/vercel-next-database/src/app/page.js
@@ -1,7 +1,23 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const posts = await sql`SELECT * FROM posts`;
+  console.log(posts);
+
   return (
     <div>
       <h1>Hello Team!</h1>
+      <h2> Here&apos;s some posts</h2>
+      {posts.rows.map((post) => {
+        return (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>
+              <em>{post.content}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
